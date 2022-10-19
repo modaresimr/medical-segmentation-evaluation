7d4ef2c039b6321790b85ea50b6993c7dcc38f3e
modaresimr <modaresi.mr@gmail.com>
2022-10-19 21:41:14 +0000

    update

--- eval_seg/common/tests/test_parallel.py
+++ eval_seg/common/tests/test_parallel.py
@@ -1,12 +1,15 @@
 import eval_seg
-import time,random
+import time, random
+
+
 def _nothing(item):
-    time.sleep(1+random.random()*2)
+    time.sleep(1 + random.random() * 2)
     return item
 
 
-def test_parallel()
-    items=range(100)
-    res=list(eval_seg.utils.parallel_runner(_nothing,items,max_chunks=10,silent=False))
-    
-    [assert items[i]==res[i] for i in range(len(items)]
+def test_parallel():
+    items = range(100)
+    res = list(eval_seg.utils.parallel_runner(_nothing, items, max_chunks=10, silent=False))
+
+    for i in range(len(items)):
+        assert items[i] == res[i]
