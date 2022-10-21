606650b2481c699e49537dd25f222f73fca46451
modaresimr <modaresi.mr@gmail.com>
2022-10-21 21:31:08 +0000

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='eval_seg',
       version='1.0',
       author='S.M.R. Modaresi',
       description='Multimodal Evaluation of Medical Image Segmentation',
-      long_description='README.md',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/modaresimr/medical-segmentation-evaluation/',
       keywords='evaluation, medical image segmentation',
       python_requires='>=3.7, <4',
-      packages=find_packages(),
+      package_dir={"": "eval_seg"}
+      packages=find_packages(where="eval_seg"),
+      namespace_packages=[],
+      zip_safe=False,
       install_requires=[
           'pandas>=0.23.3',
           'numpy>=1.14.5',
